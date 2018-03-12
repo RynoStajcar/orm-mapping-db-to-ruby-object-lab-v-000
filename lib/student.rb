f8c3d6e276fd12a449107a4983602cb13ca48ce5
Ryne Stajcar <rynostajcar@gmail.com>
2018-03-12 16:38:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,10 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
   end
 
   def self.all
@@ -41,12 +44,5 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.new_from_db
-    binding.pry
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-  end
 
 end
